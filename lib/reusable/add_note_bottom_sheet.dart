93f0d0b55b993c95c399917733695ea0b313ad34
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-07-30 01:01:56 +0300

    finish bottom sheet ui

--- lib/reusable/add_note_bottom_sheet.dart
+++ lib/reusable/add_note_bottom_sheet.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:notes_app/reusable/custom_button.dart';
 import 'package:notes_app/reusable/custom_text_field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -7,25 +8,31 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 32.0),
-        child: Column(
-          children: [
-            CustomTextField(
-              hint: 'Title',
-              maxLines: 1,
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 15.0
+    return SingleChildScrollView(
+      child: Container(
+        child: const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 32.0),
+          child: Column(
+            children: [
+              CustomTextField(
+                hint: 'Title',
+                maxLines: 1,
               ),
-              child: CustomTextField(
-                hint: 'Content',
-                maxLines: 5,
+              Padding(
+                padding: EdgeInsets.only(top: 15.0),
+                child: CustomTextField(
+                  hint: 'Content',
+                  maxLines: 5,
+                ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(
+                  top: 100.0,
+                ),
+                child: CustomButton(),
+              ),
+            ],
+          ),
         ),
       ),
     );
