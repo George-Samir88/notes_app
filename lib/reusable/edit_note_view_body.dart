67a4ff638db474271e8f62ec91e639473be53b3f
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-07-30 22:35:32 +0300

    create edit note view

---
+++ lib/reusable/edit_note_view_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class EditNoteViewBody extends StatelessWidget {
+  const EditNoteViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
--- lib/reusable/note_item.dart
+++ lib/reusable/note_item.dart
@@ -1,52 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24.0, left: 16.0, bottom: 24.0),
-      decoration: BoxDecoration(
-        color: Colors.yellow,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              'Flutter Tips',
-              style: TextStyle(color: Colors.black, fontSize: 26.0),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context,
+            MaterialPageRoute(builder: (context) => const EditNoteView()));
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24.0, left: 16.0, bottom: 24.0),
+        decoration: BoxDecoration(
+          color: Colors.yellow,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                'Flutter Tips',
+                style: TextStyle(color: Colors.black, fontSize: 26.0),
+              ),
+              subtitle: Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+                child: Text(
+                  'build your career with george samir',
+                  style: TextStyle(
+                      color: Colors.black.withOpacity(0.5), fontSize: 18.0),
+                ),
+              ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.delete,
+                ),
+                iconSize: 24.0,
+                color: Colors.black,
+              ),
             ),
-            subtitle: Padding(
-              padding: const EdgeInsets.only(top: 16.0 , bottom: 16.0),
+            Padding(
+              padding: const EdgeInsets.only(right: 24.0),
               child: Text(
-                'build your career with george samir',
+                '19/7/2023',
                 style: TextStyle(
-                    color: Colors.black.withOpacity(0.5), fontSize: 18.0),
+                    color: Colors.black.withOpacity(0.5), fontSize: 16.0),
               ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.delete,
-              ),
-              iconSize: 24.0,
-              color: Colors.black,
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 24.0),
-            child: Text(
-              '19/7/2023',
-              style: TextStyle(
-                color: Colors.black.withOpacity(0.5),
-                fontSize: 16.0
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
