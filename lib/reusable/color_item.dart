a6a6177102e32ef85f48ed956406ba2ecb8a4db2
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-08-03 01:24:22 +0300

    selecting a color with a flag

--- lib/reusable/color_item.dart
+++ lib/reusable/color_item.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isSelected});
+
+  final bool isSelected;
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(top: 15.0),
-      child: CircleAvatar(
-        backgroundColor: Colors.red,
-        radius: 36.0,
-      ),
+    return Padding(
+      padding: const EdgeInsets.only(top: 15.0),
+      child: isSelected
+          ? const CircleAvatar(
+              radius: 34.0,
+              backgroundColor: Colors.white,
+              child: CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 28.0,
+              ),
+            )
+          : const CircleAvatar(
+              backgroundColor: Colors.red,
+              radius: 28.0,
+            ),
     );
   }
 }
--- lib/reusable/colors_list_view.dart
+++ lib/reusable/colors_list_view.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:notes_app/reusable/color_item.dart';
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  late bool isSelected;
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 38*2,
+      height: 38 * 2,
       child: ListView.separated(
-        physics: const BouncingScrollPhysics(),
+          physics: const BouncingScrollPhysics(),
           scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) => const ColorItem(),
+          itemBuilder: (context, index) => GestureDetector(
+                child: ColorItem(isSelected: currentIndex == index),
+                onTap: () {
+                  currentIndex = index;
+                  setState(() {});
+                },
+              ),
           separatorBuilder: (context, index) => const SizedBox(
                 width: 10.0,
               ),
