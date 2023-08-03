85a0890fc060b49b0f75d397e6cdc2b7d1542967
George-Samir <109157277+George-Samir88@users.noreply.github.com>
2023-08-03 09:04:54 +0300

    colors list view part 2

--- lib/reusable/color_item.dart
+++ lib/reusable/color_item.dart
@@ -1,25 +1,26 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key, required this.isSelected});
+  const ColorItem({super.key, required this.isSelected, required this.color});
 
   final bool isSelected;
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(top: 15.0),
       child: isSelected
-          ? const CircleAvatar(
+          ? CircleAvatar(
               radius: 34.0,
               backgroundColor: Colors.white,
               child: CircleAvatar(
-                backgroundColor: Colors.red,
+                backgroundColor: color,
                 radius: 28.0,
               ),
             )
-          : const CircleAvatar(
-              backgroundColor: Colors.red,
+          : CircleAvatar(
+              backgroundColor: color,
               radius: 28.0,
             ),
     );
--- lib/reusable/colors_list_view.dart
+++ lib/reusable/colors_list_view.dart
@@ -11,25 +11,33 @@ class ColorsListView extends StatefulWidget {
 class _ColorsListViewState extends State<ColorsListView> {
   late bool isSelected;
   int currentIndex = 0;
+  List<Color> colors = const [
+    Color(0xffFFC6AC),
+    Color(0xffFFF6DC),
+    Color(0xffC4C1A4),
+    Color(0xff9E9FA5),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 38 * 2,
       child: ListView.separated(
-          physics: const BouncingScrollPhysics(),
-          scrollDirection: Axis.horizontal,
-          itemBuilder: (context, index) => GestureDetector(
-                child: ColorItem(isSelected: currentIndex == index),
-                onTap: () {
-                  currentIndex = index;
-                  setState(() {});
-                },
-              ),
-          separatorBuilder: (context, index) => const SizedBox(
-                width: 10.0,
-              ),
-          itemCount: 10),
+        physics: const BouncingScrollPhysics(),
+        scrollDirection: Axis.horizontal,
+        itemBuilder: (context, index) => GestureDetector(
+          child: ColorItem(
+              isSelected: currentIndex == index, color: colors[index]),
+          onTap: () {
+            currentIndex = index;
+            setState(() {});
+          },
+        ),
+        separatorBuilder: (context, index) => const SizedBox(
+          width: 10.0,
+        ),
+        itemCount: colors.length,
+      ),
     );
   }
 }
